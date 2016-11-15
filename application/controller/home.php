4ac08a1962d355e381c3ac408d8fbd52a7888371
Brett Richard Nitschke <bnitschk@mail.sfsu.edu>
2016-11-15 12:51:35 -0800

    linked search to listings page

--- application/controller/home.php
+++ application/controller/home.php
@@ -25,10 +25,10 @@ class Home extends Controller {
           $this->model->search($_POST['search_value']);
         }
 
-        $query = $this->model->search($_POST['search_value']);
+        $results = $this->model->search($_POST['search_value']);
         
         require APP . 'view/_templates/header.php';
-        require APP . 'view/home/search.php';
+        require APP . 'view/home/listings.php';
         require APP . 'view/signup/popupsignup.php';
         require APP . 'view/login/popuplogin.php';
         require APP . 'view/_templates/footer.php';
