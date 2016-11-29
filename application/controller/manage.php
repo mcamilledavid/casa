c21e3f4de2765a0aed99418130134660cf75475b
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2016-11-28 23:16:08 -0800

    Implement updateAvailability in Manage Listing page
    Manage::getUrlParameter()
    -separe url parsing logic into separate helper function
    
    Manage:updateAvailability()
    -parse rental_unit_id from URL
    
    Model::updateAvailability($ruid)
    -set is_rented for rental unit

--- application/controller/manage.php
+++ application/controller/manage.php
@@ -28,16 +28,11 @@ class Manage extends Controller {
         require APP . 'view/_templates/footer.php';
         
     }
-
+    
     public function deleteRentalUnit() {
         if(!empty($_SESSION)) {
-            // split URL (manage/deleteRentalUnit/rental_unit_id)
-            $url = trim($_GET['url'], '/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url);
-            
-            $rental_unit_id = filter_var($url[2], FILTER_SANITIZE_NUMBER_INT);
-
+            $url_param = Manage::getUrlParameter();
+            $rental_unit_id = filter_var($url_param, FILTER_SANITIZE_NUMBER_INT);
             $this->model->deleteRentalUnit($rental_unit_id);
             
             header('location: ' . URL . 'manage');
@@ -47,4 +42,32 @@ class Manage extends Controller {
         }
     }
     
+    public function updateAvailability() {
+        if(!empty($_SESSION)) {
+            $url_param = Manage::getUrlParameter();
+            $rental_unit_id = filter_var($url_param, FILTER_SANITIZE_NUMBER_INT);
+            $this->model->updateAvailability($rental_unit_id);
+            
+            header('location: ' . URL . 'manage');
+        }
+        else {
+            header('location: ' . URL . 'home');
+        }
+    }
+    
+    // helper function for parsing a parameter passed via URL
+    private static function getUrlParameter() {
+        $retval = NULL;
+        
+        if(filter_has_var(INPUT_GET, 'url')) {
+            // split URL (Controller/Action/Parameter)
+            $url = trim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            
+            $retval = $url[2];
+        }
+        
+        return $retval;
+    }
 }
--- application/model/model.php
+++ application/model/model.php
@@ -203,6 +203,14 @@ class Model {
         return $status;
     }
     
+    // set the is_rented field for a given rental unit
+    public function updateAvailability($ruid) {
+        $sql = "UPDATE rental_unit SET is_rented = 1 WHERE rental_unit_id = $ruid";
+        $query = $this->db->prepare($sql);
+        $status = $query->execute();
+        return $status;
+    }
+    
     public function getRentalUnitsByUserId($listerId) {
         $sql = "SELECT * FROM rental_unit WHERE lister_id = $listerId;";
         $query = $this->db->prepare($sql);
