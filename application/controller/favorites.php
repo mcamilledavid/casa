2935a9d08a9d91ccbdfcee8c176fb3b3308bb7a8
Brett Richard Nitschke <bnitschk@mail.sfsu.edu>
2016-11-30 09:59:39 -0800

    take 4

--- application/controller/favorites.php
+++ application/controller/favorites.php
@@ -10,12 +10,14 @@ class Favorites extends Controller {
         if (empty($_SESSION)) {
             require APP . 'view/_templates/header.php';
             require APP . 'view/signup/index.php';
+            require APP . 'view/message/popupmessage.php';
             require APP . 'view/signup/popupsignup.php';
             require APP . 'view/login/popuplogin.php';
             require APP . 'view/_templates/footer.php';
         } else {
             $favorites = $this->model->getFavoritesByUserID($_SESSION['user_id']);
             require APP . 'view/_templates/header.php';
+            require APP . 'view/message/popupmessage.php';
             require APP . 'view/account/favorites.php';
             require APP . 'view/_templates/footer.php';
         }
