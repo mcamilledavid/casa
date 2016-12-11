7d8158897319ac8047d972880e0e572b237fe365
Christopher Maxwell Dea <cdea@mail.sfsu.edu>
2016-12-11 13:29:21 -0800

    Implementing google maps

--- application/controller/gmaps.php
+++ application/controller/gmaps.php
@@ -6,10 +6,10 @@ class Gmaps extends Controller {
 
     public function index() {
 
-        require APP . 'view/_templates/header.php';
+        //require APP . 'view/_templates/header.php';
         require APP . 'view/gmaps/index.php';
-        require APP . 'view/signup/popupsignup.php';
-        require APP . 'view/login/popuplogin.php';
+        //require APP . 'view/signup/popupsignup.php';
+        //require APP . 'view/login/popuplogin.php';
         require APP . 'view/_templates/footer.php';
     }
 
